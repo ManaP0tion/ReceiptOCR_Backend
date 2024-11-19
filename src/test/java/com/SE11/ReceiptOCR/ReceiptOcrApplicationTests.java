208e6cb3cc0086fcab2ad5cbedf9a50eea836f4c
ManaP0tion <aer0dev7@gmail.com>
2024-11-19 16:22:51 +0900

    init

---
+++ src/test/java/com/SE11/ReceiptOCR/ReceiptOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SE11.ReceiptOCR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptOcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
