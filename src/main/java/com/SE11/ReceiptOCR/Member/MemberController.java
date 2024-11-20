48af2241e25beaddff79e00e81a23f4bf8e3e2b3
ManaP0tion <aer0dev7@gmail.com>
2024-11-20 11:29:27 +0900

    멤버 db관련 내용 추가

---
+++ src/main/java/com/SE11/ReceiptOCR/Member/MemberController.java
@@ -0,0 +1,32 @@
+package com.SE11.ReceiptOCR.Member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public String addMember(String user_id,
+                            String name,
+                            String email,
+                            String password) {
+        Member member = new Member();
+        member.setUser_id(user_id);
+        member.setName(name);
+        member.setEmail(email);
+        var hash = passwordEncoder.encode(password);
+        member.setPassword(hash);
+
+        memberRepository.save(member);
+
+        return "ok";        // dummy
+    }
+}
