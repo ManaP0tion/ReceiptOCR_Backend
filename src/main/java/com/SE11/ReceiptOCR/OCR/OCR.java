a5d32900c8ec43d365674e4229b18246d3c2c336
ManaP0tion <aer0dev7@gmail.com>
2024-12-11 19:53:03 +0900

    주석추가

--- src/main/java/com/SE11/ReceiptOCR/OCR/OCR.java
+++ src/main/java/com/SE11/ReceiptOCR/OCR/OCR.java
@@ -1,17 +1,16 @@
 package com.SE11.ReceiptOCR.OCR;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "ocr_results") // 테이블 이름 지정
 public class OCR {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id; // 기본 키
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성 기본 키
+    private Long id; // 기본 키                == > Long타입으로 변경했음
 
+    @Column(nullable = false) // null 불가 제약 조건
     private String textResult; // OCR 결과 텍스트
 
     // 기본 생성자
@@ -23,11 +22,11 @@ public class OCR {
     }
 
     // Getter 및 Setter 메소드
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
